62fffd4664f840f50d526d4751b04933683a86bb
snevreka <snevreka@LIN25005240.corp.capgemini.com>
2021-08-16 15:51:32 +0530

    updated

--- sampleapp/src/main/java/sampleapp/SalesClass.java
+++ sampleapp/src/main/java/sampleapp/SalesClass.java
@@ -1,9 +1,14 @@
 package sampleapp;
 
 public class SalesClass {
-	public void display() {
+	public static void display() {
 		int[] arr= {1,5,78,34,82};
 		for (int num:arr)
 			System.out.println(num);
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("Printing numbers");
+		display();
 	}
 }
