73938d4015c3f0d18cba1aecf9617804360bdd1f
snevreka <snevreka@LIN25005240.corp.capgemini.com>
2021-08-17 10:55:28 +0530

    Sum added to SalesClass

--- sampleapp/src/main/java/sampleapp/SalesClass.java
+++ sampleapp/src/main/java/sampleapp/SalesClass.java
@@ -3,8 +3,13 @@ package sampleapp;
 public class SalesClass {
 	public static void display() {
 		int[] arr= {1,5,78,34,82};
+		int sum = 0;
 		for (int num:arr)
+		{
+			sum+=num;
 			System.out.println(num);
+		}
+		System.out.println(sum);
 	}
 	public static void main(String[] args)
 	{
